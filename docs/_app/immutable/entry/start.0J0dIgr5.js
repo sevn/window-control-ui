a29e43d8311834722d0137c9069fa97a92ea680c
Claudio Holanda Jr <claudiohw@gmail.com>
2024-02-07 20:01:53 -0300

    feat: changing base path to docs

---
+++ docs/_app/immutable/entry/start.0J0dIgr5.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.kX815ZC8.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			base: "/docs"
+		},
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
